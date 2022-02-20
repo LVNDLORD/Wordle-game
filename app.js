d97c69ae9ff283c484eb8390f27c8c87dcc38b56
Andrey <andydeshko@gmail.com>
2022-02-20 15:43:55 +0200

    create tiles & set id for each row and tile

--- app.js
+++ app.js
@@ -11,10 +11,31 @@ keys.splice(19, 0, 'Enter');
 keys.push('«'); //backspace
 console.log(keys);
 
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+]
+
+// setting id for each row and tile
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowEl = document.createElement('div');
+    rowEl.setAttribute('id', 'guessRow-' + guessRowIndex);
+    guessRow.forEach((guess, guessIndex) => {
+        const tileEl = document.createElement('div');
+        tileEl.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex);
+        tileEl.classList.add('tile');
+        rowEl.append(tileEl);
+    })
+    tileDisplay.append(rowEl);
+});
+
 const handleClick = () => {
     console.log('clicked');
 }
-
 
 keys.forEach(key => {
     const buttonEl = document.createElement('button');

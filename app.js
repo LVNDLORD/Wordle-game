4aeb54c65dcda30b60c632baa348e8d34cf208c7
Andrey <andydeshko@gmail.com>
2022-02-20 14:54:02 +0200

    created keyboard

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ const keys = Array.from(allKeyboardCharacters);
 // const keyPressed = e => console.log(e.key);
 // document.addEventListener('keydown', keyPressed);
 keys.splice(19, 0, 'Enter');
-keys.push('Backspace');
+keys.push('«'); //backspace
 console.log(keys);
+
+const handleClick = () => {
+    console.log('clicked');
+}
 
 
 keys.forEach(key => {
     const buttonEl = document.createElement('button');
     buttonEl.textContent = key;
+    buttonEl.setAttribute('id', key);
+    buttonEl.addEventListener('click', handleClick)
     keyboard.append(buttonEl);
 });
 
